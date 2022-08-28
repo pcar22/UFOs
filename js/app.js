6566c7b6785b690cb338017a8ceb4c61b4f39e36
pcar22 <pcarranza2@comcast.net>
2022-08-28 10:36:29 -0600

    correcting issues

--- js/app.js
+++ js/app.js
@@ -4,41 +4,36 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-// First, lcear out any existing data
+    // First, clear out any existing data
     tbody.html("");
+  
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
-   data.forEach((datarow) => {
-    // Append a row to the table body
-    let row = tbody.append("tr");
-
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(datatRow).forEach((val) => {
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
         cell.text(val);
-   
-        }        
-    );    
-   
-   });
+        }
+      );
+    });
 }
 
-
-function handleClick() {
-
-    
+function handleClick() {    
 
     let date = d.select("#datetime").property("value");
     let filteredData = tableData;
 
 // Check to see if a date was entered and filter the 
-// data using that date.    
+// data using that date.   pseudocode practice if (a date is entered) {Filter the default data to show only the date entered}; 
 if (date) {
         // Apply `filter` to the table data to only keep the 
     // rows where the `datetime` value matches the filter value
     filteredData = filteredData.filter(row => row.datetime === date);
-
 }
 
     // Rebuild the tabel using the filtered data
